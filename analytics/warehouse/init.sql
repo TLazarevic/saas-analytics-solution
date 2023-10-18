49a8e43e61b46dc0c61c7eea6b7285875dc71b32
Tamara Lazarevic <lazarevict97@gmail.com>
2023-10-18 20:21:35 +0200

    Add event tracking (jitsu) (#3)
    * Add event tracking (jitsu)
    
    * Update diagram
    
    * Minor improvements

--- analytics/warehouse/init.sql
+++ analytics/warehouse/init.sql
@@ -8,6 +8,18 @@ GRANT SELECT ON analytics.* TO analytics;
 GRANT INSERT ON analytics.* TO analytics;
 GRANT TRUNCATE ON analytics.* TO analytics;
 
+-- jitsu
+CREATE DATABASE IF NOT EXISTS events;
+CREATE USER OR REPLACE jitsu IDENTIFIED WITH plaintext_password BY 'jitsu';
+
+GRANT CREATE DATABASE ON events.* TO jitsu;
+GRANT CREATE TABLE ON events.* TO jitsu;
+GRANT DROP TABLE ON events.* TO jitsu;
+GRANT SELECT ON events.* TO jitsu;
+GRANT INSERT ON events.* TO jitsu;
+GRANT TRUNCATE ON events.* TO jitsu;
+
 -- metabase
 CREATE USER OR REPLACE metabase;
 GRANT SELECT ON analytics.* TO metabase;
+GRANT SELECT ON events.* TO metabase;
