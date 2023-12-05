ba650fe384ce3dde723208b2e8b0e219c8adddcb
TLazarevic <lazarevict97@gmail.com>
2023-12-05 22:24:29 +0100

    Add undroppable new card and new column divs

--- saas-app/app/kandy/public/javascripts/boards.js
+++ saas-app/app/kandy/public/javascripts/boards.js
@@ -1,25 +1,14 @@
-import Droppable from 'https://cdn.jsdelivr.net/npm/@shopify/draggable/build/esm/Draggable/Draggable.mjs';
-import Draggable from 'https://cdn.jsdelivr.net/npm/@shopify/draggable/build/esm/Draggable/Draggable.mjs';
 import Sortable from 'https://cdn.jsdelivr.net/npm/@shopify/draggable/build/esm/Sortable/Sortable.mjs';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var options = {
-        group: 'share',
-        animation: 100
-    };
 
-    var el = document.getElementById('cards');
-    // const draggable = new Draggable(document.querySelectorAll('tr'), {
-    //     draggable: 'td',
-    // });
-    // const droppable = new Droppable(document.querySelectorAll('tbody'), {
-    //     draggable: '.item',
-    //     dropzone: 'tbody',
-    // });
     const sortable = new Sortable(
-        document.querySelectorAll('.column'), {
+        document.querySelectorAll('.sortable_column'), {
             draggable: '.sortItem',
             delay: 0,
+            mirror: {
+              constrainDimensions: true,
+            },
         }
     )
 
@@ -35,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('sortable:sorted');
     });
 
-    sortable.on('sortable:stop', () => {
-        setTimeout(updateListItems, 10);
+    sortable.on('sortable:stop', (evt) => {
+        //setTimeout(updateListItems, 10);
         console.log('sortable:stop');
     });
 });
