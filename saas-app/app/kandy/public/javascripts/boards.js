86ca675fc09675b8481b5fc144f17bc25bca83fd
Tamara Lazarevic <lazarevict97@gmail.com>
2024-01-07 21:38:55 +0100

    Board page basic functionality (#6)
    * Board page initial setup
    
    * Minimal working drag and drop example
    
    * Add undroppable new card and new column divs
    
    * Add modals for new card and column
    
    * Remove unused packages
    
    * Implement add a card and add a column modals
    
    * Finish moving cards endpoint
    
    * Implement card reordering. Implement sample test
    
    * Implement column sort and repositioning
    
    * Code cleanup

---
+++ saas-app/app/kandy/public/javascripts/boards.js
@@ -0,0 +1,100 @@
+import Sortable from 'https://cdn.jsdelivr.net/npm/@shopify/draggable/build/esm/Sortable/Sortable.mjs';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const columns = document.querySelectorAll('.sortable_column');
+    const cards = document.querySelectorAll('.sortable_card');
+
+    const sortable = new Sortable(
+        cards, {
+        draggable: '.sortCard',
+        delay: 0,
+        mirror: {
+            constrainDimensions: true
+        },
+    }
+    )
+
+    const sortableColumn = new Sortable(
+        columns, {
+        draggable: '.sortColumn',
+        handle: ".column_header",
+        delay: 0,
+        mirror: {
+            constrainDimensions: true
+        },
+    }
+    )
+
+    sortableColumn.on('sortable:stop', async (event) => {
+
+        let columnId = event.data.dragEvent.data.source.id
+        let newPosition = event.newIndex
+        let boardId = window.location.href.split('/')[4];
+
+        try {
+            let response = null
+            let body = null
+
+            body = JSON.stringify({ 'position': newPosition })
+
+            response = await fetch(`${boardId}/column/${columnId}/move`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: body
+            });
+
+            if (response.ok) {
+                console.log('Column updated successfully');
+            } else {
+                console.error('Failed to update column:', response.message);
+                // TODO Update UI
+            }
+        } catch (error) {
+            console.error('Error during update:', error);
+            // TODO Update UI
+        }
+
+    });
+
+    sortable.on('sortable:stop', async (event) => {
+
+        let cardId = event.data.dragEvent.data.source.id
+        let prevColumnId = event.oldContainer.id
+        let newColumnId = event.newContainer.id
+        let newPosition = event.newIndex
+        let boardId = window.location.href.split('/')[4];
+
+        try {
+            let response = null
+            let body = null
+
+            if (prevColumnId !== newColumnId) {
+                body = JSON.stringify({ 'columnId': newColumnId, 'position': newPosition })
+            }
+            else {
+                body = JSON.stringify({ 'position': newPosition })
+            }
+
+            response = await fetch(`${boardId}/column/${prevColumnId}/card/${cardId}/move`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: body
+            });
+
+            if (response.ok) {
+                console.log('Row updated successfully');
+            } else {
+                console.error('Failed to update row:', response.message);
+                // TODO Update UI
+            }
+        } catch (error) {
+            console.error('Error during update:', error);
+            // TODO Update UI
+        }
+    });
+});
---
+++ saas-app/app/kandy/routes/boards.js
@@ -0,0 +1,389 @@
+const { PrismaClient } = require('@prisma/client');
+const { v4: uuidv4 } = require('uuid');
+var express = require('express');
+var router = express.Router();
+
+const prisma = new PrismaClient()
+
+router.get('/:id', async (req, res) => {
+    const boardId = req.params.id;
+
+    try {
+        const board = await prisma.boards.findUnique({
+            where: { id: boardId },
+            include: {
+                columns: {
+                    orderBy: {
+                        position: 'asc'
+                    },
+                    include: {
+                        cards: {
+                            orderBy: {
+                                position: 'asc'
+                            }
+                        }
+                    },
+                },
+            },
+        });
+
+        res.render('board', { board: board });
+    } catch (error) {
+        console.error('Error retrieving board:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+router.post('/:id/column', async (req, res) => {
+    const boardId = req.params.id;
+    const name = req.body.name;
+
+    try {
+        const column = await prisma.columns.create({
+            data: {
+                id: uuidv4(),
+                name: name,
+                board_id: boardId
+            }
+        });
+
+        try {
+            const board = await prisma.boards.findUnique({
+                where: { id: boardId },
+                include: {
+                    columns: {
+                        include: {
+                            cards: true,
+                        },
+                    },
+                },
+            });
+
+            res.render('board', { board: board });
+        } catch (error) {
+            console.error('Error retrieving board:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } catch (error) {
+        console.error('Error creating column:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+router.post('/:id/column/:columnId/card', async (req, res) => {
+    const boardId = req.params.id;
+    const columnId = req.params.columnId;
+    const name = req.body.name;
+    const description = req.body.description;
+
+    try {
+        const maxPosition = await prisma.cards.aggregate({
+            where: {
+                column_id: columnId,
+            },
+            _max: {
+                position: true,
+            }
+        })
+
+        const card = await prisma.cards.create({
+            data: {
+                id: uuidv4(),
+                name: name,
+                description: description,
+                column_id: columnId,
+                position: maxPosition._max.position !== null ? maxPosition._max.position + 1 : 0
+            }
+        });
+
+        try {
+            const board = await prisma.boards.findUnique({
+                where: { id: boardId },
+                include: {
+                    columns: {
+                        include: {
+                            cards: true,
+                        },
+                    },
+                },
+            });
+
+            res.render('board', { board: board });
+        } catch (error) {
+            console.error('Error retrieving board:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } catch (error) {
+        console.error('Error creating card:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+router.patch('/:id/column/:columnId/card/:cardId/move', async (req, res) => {
+
+    const boardId = req.params.id;
+    const columnId = req.params.columnId;
+    const cardId = req.params.cardId;
+
+    const newColumnId = req.body.columnId;
+    const newPosition = req.body.position;
+
+    console.log("Moving card", cardId)
+
+    const card = await prisma.cards.findUnique({
+        where: { id: cardId }
+    })
+    const prevPosition = card.position;
+
+    let result = null
+
+    try {
+        if (!newColumnId) {
+            if (newPosition > prevPosition) {
+
+                result = await prisma.$transaction([
+
+                    prisma.cards.updateMany({
+                        where: {
+                            column_id: columnId,
+                            position: {
+                                gt: prevPosition,
+                                lte: newPosition
+                            }
+                        },
+                        data: {
+                            position: { decrement: 1 }
+                        }
+                    }),
+
+                    prisma.cards.update({
+                        where: {
+                            id: cardId
+                        },
+                        data: {
+                            position: newPosition
+                        }
+                    }),
+
+                    prisma.boards.findUnique({
+                        where: { id: boardId },
+                        include: {
+                            columns: {
+                                include: {
+                                    cards: true,
+                                },
+                            },
+                        },
+                    })
+
+                ]);
+            }
+            else {
+                result = await prisma.$transaction([
+
+                    prisma.cards.updateMany({
+                        where: {
+                            column_id: columnId,
+                            position: {
+                                gte: newPosition,
+                                lt: prevPosition
+                            }
+                        },
+                        data: {
+                            position: { increment: 1 }
+                        }
+                    }),
+
+                    prisma.cards.update({
+                        where: {
+                            id: cardId
+                        },
+                        data: {
+                            position: newPosition
+                        }
+                    }),
+
+                    prisma.boards.findUnique({
+                        where: { id: boardId },
+                        include: {
+                            columns: {
+                                include: {
+                                    cards: true,
+                                },
+                            },
+                        },
+                    })
+
+                ]);
+            }
+        }
+        else {
+
+            result = await prisma.$transaction([
+                prisma.cards.update({
+                    where: {
+                        id: cardId
+                    },
+                    data: {
+                        column_id: newColumnId
+                    }
+                }),
+
+                prisma.cards.updateMany({
+                    where: {
+                        column_id: newColumnId,
+                        position: {
+                            gte: newPosition
+                        }
+                    },
+                    data: {
+                        position: { increment: 1 }
+                    }
+                }),
+
+                prisma.cards.updateMany({
+                    where: {
+                        column_id: columnId,
+                        position: {
+                            gt: prevPosition
+                        }
+                    },
+                    data: {
+                        position: { decrement: 1 }
+                    }
+                }),
+
+                prisma.cards.update({
+                    where: {
+                        id: cardId
+                    },
+                    data: {
+                        position: newPosition
+                    }
+                }),
+
+                prisma.boards.findUnique({
+                    where: { id: boardId },
+                    include: {
+                        columns: {
+                            include: {
+                                cards: true,
+                            },
+                        },
+                    },
+                })
+
+            ]);
+        }
+
+        res.json({ success: true, result });
+    } catch (error) {
+        console.error('Error updating card or retrieving board:', error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+});
+
+router.patch('/:id/column/:columnId/move', async (req, res) => {
+
+    const boardId = req.params.id;
+    const columnId = req.params.columnId;
+
+    const newPosition = req.body.position;
+
+    console.log("Moving column", columnId)
+
+    const column = await prisma.columns.findUnique({
+        where: { id: columnId }
+    })
+    const prevPosition = column.position;
+
+    let result = null
+
+    try {
+
+        if (newPosition > prevPosition) {
+
+            result = await prisma.$transaction([
+
+                prisma.columns.updateMany({
+                    where: {
+                        board_id: boardId,
+                        position: {
+                            gt: prevPosition,
+                            lte: newPosition
+                        }
+                    },
+                    data: {
+                        position: { decrement: 1 }
+                    }
+                }),
+
+                prisma.columns.update({
+                    where: {
+                        id: columnId
+                    },
+                    data: {
+                        position: newPosition
+                    }
+                }),
+
+                prisma.boards.findUnique({
+                    where: { id: boardId },
+                    include: {
+                        columns: {
+                            include: {
+                                cards: true,
+                            },
+                        },
+                    },
+                })
+
+            ]);
+        }
+        else {
+            result = await prisma.$transaction([
+
+                prisma.columns.updateMany({
+                    where: {
+                        board_id: boardId,
+                        position: {
+                            gte: newPosition,
+                            lt: prevPosition
+                        }
+                    },
+                    data: {
+                        position: { increment: 1 }
+                    }
+                }),
+
+                prisma.columns.update({
+                    where: {
+                        id: columnId
+                    },
+                    data: {
+                        position: newPosition
+                    }
+                }),
+
+                prisma.boards.findUnique({
+                    where: { id: boardId },
+                    include: {
+                        columns: {
+                            include: {
+                                cards: true,
+                            },
+                        },
+                    },
+                })
+
+            ]);
+        }
+
+        res.json({ success: true, result });
+    } catch (error) {
+        console.error('Error updating card or retrieving board:', error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
