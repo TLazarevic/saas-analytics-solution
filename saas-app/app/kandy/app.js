9613a3a9451b81b54bc4bfdf8f18d7619018ee9f
Tamara Lazarevic <lazarevict97@gmail.com>
2024-01-13 16:13:16 +0100

    Handle 404 (#11)
    * Handle 404

--- saas-app/app/kandy/app.js
+++ saas-app/app/kandy/app.js
@@ -69,20 +69,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/boards', boardsRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.status(404).render('404');
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
+
