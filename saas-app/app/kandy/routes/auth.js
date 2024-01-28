cda14497db8baef287453c65c7219a3aed9de32d
TLazarevic <lazarevict97@gmail.com>
2024-01-28 19:22:01 +0100

    Finish login and signup

--- saas-app/app/kandy/routes/auth.js
+++ saas-app/app/kandy/routes/auth.js
@@ -75,4 +75,60 @@ router.post(
     }
 );
 
+router.post(
+    "/login",
+    [
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+        const { email, password } = req.body;
+        try {
+            let user = await prisma.users.findUnique({
+                where: { email: email }
+            });
+            if (!user)
+                return res.status(400).json({
+                    message: "The email is not tied to any accounts"
+                });
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+                return res.status(400).json({
+                    message: "Incorrect password"
+                });
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+            jwt.sign(
+                payload,
+                "secret",
+                {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
+    }
+);
+
 module.exports = router; 
