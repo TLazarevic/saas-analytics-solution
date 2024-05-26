a5794699e2b178b524993dcbfdedb5d24f71ffc3
TLazarevic <lazarevict97@gmail.com>
2024-05-26 21:18:30 +0200

    Minor improvements and demo video update

--- saas-app/app/kandy/util/analytics.js
+++ saas-app/app/kandy/util/analytics.js
@@ -1,18 +1,28 @@
 const { jitsuAnalytics } = require('@jitsu/js');
 
+const { logger } = require('structlog');
+
 const jitsu = jitsuAnalytics({
     host: "http://localhost:8080",
     writeKey: process.env.WRITE_KEY_JITSU,
 });
 
 async function track(event_name, properties) {
-    console.log("Emitting analytics event.", event_name, properties)
-    await jitsu.track(event_name, properties);
+    try {
+        logger.info("Emitting analytics event.", { "event_name": event_name });
+        return await jitsu.track(event_name, properties);
+    } catch (error) {
+        logger.error("Error emitting analytics.", error)
+    }
 }
 
 async function identify(user_id) {
-    console.log("Identifying user for analytics.", user_id)
-    await jitsu.identify(user_id);
+    try {
+        logger.info("Identifying user for analytics.", { "user_id": user_id });
+        return await jitsu.identify(user_id);
+    } catch (error) {
+        logger.error("Error emitting analytics.", error)
+    }
 }
 
 module.exports = { track, identify };
